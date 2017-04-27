95850db88cdd209b375d4966afa7b8b0f25ed176
thanh <thanhns.bkap@gmail.com>
2017-04-27 15:08:59 +0700

    update main
    update main

--- app/src/main/java/com/att/attcase/TrangChu.java
+++ app/src/main/java/com/att/attcase/TrangChu.java
@@ -8,26 +8,30 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class TrangChu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    Button btn_chuyen_trang_2,btn_chuyen_trang_3;
+    Button btn_chuyen_trang_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //getSupportActionBar().setDisplayShowHomeEnabled(false);*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+        toggle.setHomeAsUpIndicator(R.drawable.ic_menu_camera);
+      //  toggle.setDrawerIndicatorEnabled(true);     // this will disable your default haburger icon
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -44,18 +48,10 @@ public class TrangChu extends AppCompatActivity
                 startActivity(mChuyenPage2);
             }
         });
-        btn_chuyen_trang_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mChuyenPage3 =new Intent(getApplicationContext(),XayDungCase.class);
-                startActivity(mChuyenPage3);
-            }
-        });
     }
 
     private void addControls() {
         btn_chuyen_trang_2= (Button) findViewById(R.id.btn_chuyen_trang_2);
-        btn_chuyen_trang_3= (Button) findViewById(R.id.btn_chuyen_trang_3);
     }
 
     @Override
@@ -68,46 +64,35 @@ public class TrangChu extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.trang_chu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_home) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
         } else if (id == R.id.nav_gallery) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_cart) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_invite) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
+        } /*else if (id == R.id.nav_log_out) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
 
-        } else if (id == R.id.nav_slideshow) {
+        }*/ else if (id == R.id.nav_make_case) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
+        }else if (id == R.id.nav_profile) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
 
-        } else if (id == R.id.nav_manage) {
+        }else if (id == R.id.nav_promotion) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        }else if (id == R.id.nav_store) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
+        }else if (id == R.id.nav_view) {
+            Toast.makeText(getApplicationContext(),"Tính năng sẽ được update trong thời gian sớm nhất",Toast.LENGTH_LONG).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
